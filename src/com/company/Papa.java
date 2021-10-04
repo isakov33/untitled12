dff05439a619b416030bfdc5f80257c318f24f2f
isakov33 <osipov.evgeny.htcococ@gmail.com>
2021-10-04 16:58:32 +0600

    Initial commit

---
+++ src/com/company/Papa.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Papa extends Ded{
+    public Papa(String name, int age, String face, Hobie hobie) {
+        super(name, age, face, hobie);
+
+    }
+    public void goToZal(int time, String Adrees ) {
+        return;
+    }
+
+    public void goToZal(String days) {
+        return;
+    }
+
+    public final void goToZal(int turnik) {
+        return;
+
+    }
+
+}
